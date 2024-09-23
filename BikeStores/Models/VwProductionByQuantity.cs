1defa66268cfe0e62b6efb94c65630c165b7ddeb
Patrick Casale <patrick.casale@edu.itspiemonte.it>
2024-09-23 10:38:31 +0200

    Installazione pacchetti necessari e scuffolding

---
+++ BikeStores/Models/VwProductionByQuantity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BikeStores.Models;
+
+public partial class VwProductionByQuantity
+{
+    public string CategoryName { get; set; } = null!;
+
+    public string ProductName { get; set; } = null!;
+
+    public short ModelYear { get; set; }
+
+    public decimal ListPrice { get; set; }
+
+    public int? Quantity { get; set; }
+}
---
+++ BikeStores/Models/VwProductsByCustomer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BikeStores.Models;
+
+public partial class VwProductsByCustomer
+{
+    public string ProductName { get; set; } = null!;
+
+    public string BrandName { get; set; } = null!;
+
+    public string CategoryName { get; set; } = null!;
+
+    public int PurchaseQuantity { get; set; }
+
+    public decimal? DiscountedPrice { get; set; }
+
+    public decimal Discount { get; set; }
+
+    public DateOnly OrderDate { get; set; }
+
+    public string CustomereName { get; set; } = null!;
+
+    public string CustomereLastName { get; set; } = null!;
+
+    public string? City { get; set; }
+}
